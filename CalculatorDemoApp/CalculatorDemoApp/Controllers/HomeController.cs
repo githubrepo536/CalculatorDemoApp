bd3e790df94c32e5bdaffeadd76b3a71c203e455
githubrepo536 <mausha.gottipati@gmail.com>
2021-05-29 12:43:25 -0400

    calculateapp

---
+++ CalculatorDemoApp/CalculatorDemoApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using CalculatorDemoApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorDemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string firstNumber, string secondNumber, string Cal)
+        {
+            int a = Convert.ToInt32(firstNumber);
+            int b = Convert.ToInt32(secondNumber);
+            int c = 0;
+            switch (Cal)
+            {
+                case "Add":
+                    c = a + b;
+                    break;
+                case "Sub":
+                    c = a - b;
+                    break;
+                case "Mul":
+                    c = a * b;
+                    break;
+                case "Div":
+                    c = a / b;
+                    break;
+            }
+            ViewBag.Result = c;
+            return View();
+        }
+    }
+}
